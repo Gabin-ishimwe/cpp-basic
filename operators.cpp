3cd2c9c8351d731aa6215f8ab3c374aadf195e45
gabin-AF <gabin@augmented-future.com>
2022-10-22 10:53:11 +0200

    operators

--- operators.cpp
+++ operators.cpp
@@ -3,7 +3,13 @@
 int main() {
   int x = 3;
   int y = x + 3;
+  int z = x / 3;
+  int w = x - 3;
+  int m = x % 3;
   std::cout << y << std::endl;
+  std::cout << z << std::endl;
+  std::cout << w << std::endl;
+  std::cout << m << std::endl;
 
   return 0;
 }
